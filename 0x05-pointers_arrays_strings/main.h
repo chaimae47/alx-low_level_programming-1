cdf5203aaab3d1d89f9462ccdb328dc9f44511d3
ymatab <136011049+ymatab@users.noreply.github.com>
2023-07-25 11:42:01 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
-
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,8 +6,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+int _putchar(char);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-
-#endif
-
+int _atoi(char *s);
